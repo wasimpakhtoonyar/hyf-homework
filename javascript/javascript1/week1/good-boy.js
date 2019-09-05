2b73b3f62bc5359737cafbffe48cbfb0cbc6308e
wasimpakhtoonyar <52453997+wasimpakhtoonyar@users.noreply.github.com>
2019-09-05 16:03:09 +0200

    Create good-boy.js

---
+++ javascript/javascript1/week1/good-boy.js
@@ -0,0 +1,19 @@
+console.log('good boy');
+
+const dogYearOfBirth = 2000;
+let dogYearFuture = 2020;
+
+let dogYear = dogYearFuture - dogYearOfBirth;
+
+let shouldShowResultInDogYears = true;
+
+if (shouldShowResultInDogYears) {
+    let result = dogYear * 3;
+    console.log("Your dog will be " + result + " dog years old in " + dogYearFuture);
+} else {
+    console.log("Your dog will be " + dogYear + 7 +" human years old in " + 60);
+}
+console.log("\n-- Goodboy-Oldboy (A dog age calculator) --");
+if(shouldShowResultInDogYears) {
+  console.log(str2);
+} else console.log(str)
