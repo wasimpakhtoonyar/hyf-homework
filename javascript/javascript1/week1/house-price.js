d964ea9255e7db409fcd7f7b02bfe039fbc116b0
wasimpakhtoonyar <52453997+wasimpakhtoonyar@users.noreply.github.com>
2019-09-30 21:30:12 +0200

    Update house-price.js

--- javascript/javascript1/week1/house-price.js
+++ javascript/javascript1/week1/house-price.js
@@ -1,33 +1,15 @@
-console.log('house price');
+let gardenSizeInM2 = 100;
+let width = 5;
+let height = 11;
+let depth = 8;
+let houseCost = 1000000;
+const volumeInMeters = width * height * depth;
 
-let wideInMetersForPeter = 8;
-let deepInMetersForPeter = 10;
-let highInMetersForPeter = 10;
-let gardenSizeInM2ForPeter = 100;
+const housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+console.log ("\n-- Housey pricey (A house price estimator) --");
+console.log("How much you paid: " + houseCost);
+console.log("How much the house should cost: " + housePrice);
 
-let volumeInMetersForPeter = wideInMetersForPeter * deepInMetersForPeter * highInMetersForPeter;
-let housePriceForPeter = volumeInMetersForPeter * 2.5 * 1000 + gardenSizeInM2ForPeter * 300;
-
-console.log("House price for Peter: " + housePriceForPeter);
-
-if (housePriceForPeter > 2500000) {
-    console.log("The house is expensive");
-} else {
-    console.log("The price is okay");
-}
-
-
-let wideInMetersForJulia = 5;
-let deepInMetersForJulia = 11;
-let highInMetersForJulia = 8;
-let gardenSizeInM2ForJulia = 70;
-
-let volumeInMetersForJulia = wideInMetersForJulia * deepInMetersForJulia * highInMetersForJulia;
-let housePriceForJulia = volumeInMetersForJulia * 2.5 * 1000 + gardenSizeInM2ForJulia * 300;
-
-console.log("House price for Julia: " + housePriceForJulia);
-if (housePriceForJulia > 1121000') {
-    console.log("The house is expensive");
-} else {
-    console.log("The price is okay");
-}
+if(housePrice > houseCost) {
+    console.log("You are paying too little. Congrats!")
+} else console.log("You are paying too much.")
