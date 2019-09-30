424a2f06a9456c6bb919214597f83c817e54b63e
wasimpakhtoonyar <52453997+wasimpakhtoonyar@users.noreply.github.com>
2019-09-30 22:19:54 +0200

    Update age-ify.js

--- javascript/javascript1/week1/age-ify.js
+++ javascript/javascript1/week1/age-ify.js
@@ -4,5 +4,6 @@ const yearOfBirth = 1988;
 let yearFuture = 2020;
 
 let age = yearFuture - yearOfBirth;
+console.log("-- Age-ify (A future age calculator) --")
 
-console.log("You will be " + 32 + " years old in " + 2020);
+console.log("You will be " + age + " years old in " + yearFuture);
